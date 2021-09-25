1dab81897ba37377053a2f0642ea7c6393acc495
Thaumy <Studio_ai@outlook.com>
2021-09-25 16:01:29 +0800

    Update .gitignore

--- unitest/cmake-build-debug/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/project/proj/cpp/yparser proj/unitest/lib/googlemock
-# Build directory: F:/project/proj/cpp/yparser proj/unitest/cmake-build-debug/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
